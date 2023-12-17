4e5506328bfa4d32edcf55a6db4552377a6fde46
jason810496 <810496@email.wlsh.tyc.edu.tw>
2023-12-17 14:02:33 +0800

    fix build public path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: '/BikeFestival17th-Frontend/'
+  base: '/BikeFestival17th-Frontend/',
 })
