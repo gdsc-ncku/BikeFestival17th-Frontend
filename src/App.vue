2b2261f9e91351c1be033433854ddcee21436b56
yeeway <alex20020609@gmail.com>
2024-01-10 00:55:11 +0800

    Add Tailwind CSS configuration and customized color palette

--- src/App.vue
+++ src/App.vue
@@ -14,17 +14,6 @@ import HelloWorld from './components/HelloWorld.vue'
   <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+  @import './style.css';
 </style>
