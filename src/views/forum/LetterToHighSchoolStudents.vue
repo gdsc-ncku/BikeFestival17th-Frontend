dd14fb60d6a37dd5bdcaeacc96cb26be28cc8aca
yeeway <alex20020609@gmail.com>
2024-01-14 17:59:50 +0800

    add: add data files, add new views and their routes

---
+++ src/views/forum/LetterToHighSchoolStudents.vue
@@ -0,0 +1,7 @@
+<template>
+  <h1 class="text-3xl font-bold">給高中生的一封信</h1>
+  <h1 class="text-3xl font-bold">給高中生的一封信</h1>
+  <h1 class="text-3xl font-bold">給高中生的一封信</h1>
+  <h1 class="text-3xl font-bold">給高中生的一封信</h1>
+</template>
+
