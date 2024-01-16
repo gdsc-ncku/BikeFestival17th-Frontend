57b45a14bafd63298a17c2a0b22ccc42c1d6e1f1
CHIH-HAO, CHUANG <contact@chih-hao.xyz>
2024-01-16 17:05:54 +0800

    add: Disc Card

--- src/views/forum/FeaturedArticles.vue
+++ src/views/forum/FeaturedArticles.vue
@@ -3,5 +3,9 @@
   <h1 class="text-3xl font-bold">專欄文章</h1>
   <h1 class="text-3xl font-bold">專欄文章</h1>
   <h1 class="text-3xl font-bold">專欄文章</h1>
+  <DiscCard title="真假新聞分不清？不私藏網路新聞指南！" title-size="text-base" />
 </template>
 
+<script setup>
+import DiscCard from '../../components/DiscCard.vue';
+</script>
