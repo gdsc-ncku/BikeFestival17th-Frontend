a28790cfd56f872ac37aefb0506e6d12a4d3695e
yeeway <yiwei.suuu@gmail.com>
2024-01-20 01:18:08 +0800

    fix: Fix NavBar links and update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/BikeFestival17th-Frontend/' : '/',
+  base: '/BikeFestival17th-Frontend/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
