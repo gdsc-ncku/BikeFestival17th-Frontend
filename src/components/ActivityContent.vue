8a19946eced114251dae78f5dde8d14fe63fc9b5
YeyeLeaf <ling930718@gmail.com>
2024-01-21 00:59:52 +0800

    add: Traffic & Map pages and activities img effect

--- src/components/ActivityContent.vue
+++ src/components/ActivityContent.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="lg:max-w-[1200px] max-w-[360px] mx-auto">
+  <div class="lg:max-w-[1200px] max-w-[370px] mx-auto">
     <div class="bg-white rounded-2xl px-5 py-3 h-fit min-w-[250px] lg:fixed mb-10">
       <h1 class="font-bold text-primary text-2xl mb-5">{{ props.title }}</h1>
       <a @click="setNum(index)" :href="'#item_' + index" v-for="(item, index) in list" :key="index"><h2 class="text-content text-xl mb-5 hover:bg-[#FFE0CA] rounded-lg p-1" :class="num === index ? 'bg-[#FFE0CA]' : ''">{{ item.subtitle }}</h2></a>
     </div>
     <div class="lg:w-3/4 lg:ml-[300px]">
-      <div class="relative">
-        <img :src="img" class="rounded-2xl object-cover max-h-[400px] w-full mb-[400px] absolute z-0" />
-        <h1 class="text-white lg:text-5xl text-4xl font-bold absolute z-10 lg:top-80 top-40 lg:right-10 lg:left-auto left-5">{{ props.title }}</h1>
+      <div class="relative w-full lg:h-[400px] h-[200px] mb-5">
+        <img :src="img" class="rounded-2xl object-cover w-full mb-[400px] absolute h-full" />
+        <div style="border-radius: 16px;
+background: linear-gradient(0deg, #404040 0%, rgba(217, 217, 217, 0.00) 100%);" class="absolute z-10 w-full h-full"></div>
+        <h1 class="text-white lg:text-5xl text-4xl font-bold absolute z-10 lg:top-80 top-32 lg:right-10 lg:left-auto left-5">{{ props.title }}</h1>
       </div>
-      <div class="lg:h-[420px] h-[240px]"></div>
+      
       <div v-for="(item, index) in props.list" :key=index class="text-content text-xl mb-72">
         <h2 :id="'item_' + index" class="text-3xl font-bold mb-5 bg-[#FF5C0045] w-auto inline-block">{{ item.subtitle }}</h2>
         <h3 class="text-2xl font-bold mb-5">活動介紹</h3>
---
+++ src/views/info/Traffic.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="lg:w-7/12 w-full">
+    <h2 class="text-primary font-bold lg:text-4xl text-xl py-5">開車</h2>
+    <p class="lg:text-xl text-lg text-content"><span class="font-bold">南下</span><br>走中山高速公路南下 → 於永康交流道下高速公路 → 走中正南路(西向)往台南市區 → 轉中華東路 → 達小東路口右轉(西向)直走便可抵成大校區。<br><br><span class="font-bold">北上</span><br>
+  走中山高速公路北上 → 於仁德交流道下高速公路 → 走東門路(西向)往台南市區 → 直走遇長榮路右轉(北向)可抵自強、成功、敬業、建國校區 → 東門路過長榮路遇勝利路右轉(北向)直走可抵光復校門口。</p>
+    <h2 class="text-primary font-bold lg:text-4xl text-xl py-5">大眾交通</h2>
+    <p class="lg:text-xl text-lg text-content">台南火車站下車後，往後站方向，出口正對面即為大學路，大學路直走左手邊可見光復校門口。<br>
+  搭乘客運在台南火車站前站下車者， 可經由火車站天橋前往後站，後站出口左手側即為成大光復校區。</p>
+  </div>
+  <img src="" alt="map" class="w-full lg:h-[700px] h-[300px] bg-gray-500 my-20">
+</template>
