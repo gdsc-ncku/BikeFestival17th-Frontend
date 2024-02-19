78e70acfccca1657d287d6df9e3e99a0cd762632
yeeway <yiwei.suuu@gmail.com>
2024-02-19 20:53:57 +0800

    Add WorkInProgress component to LetterToHighSchoolStudents.vue

--- src/views/forum/LetterToHighSchoolStudents.vue
+++ src/views/forum/LetterToHighSchoolStudents.vue
@@ -2,10 +2,11 @@
   <Breadcrumb />
   <div class="mx-[9.75rem]">
     <div class="text-5xl font-bold mb-8">給高中生的一封信</div>
-    <div class="text-2xl mb-8">123456</div>
+    <WorkInProgress />
   </div>
 </template>
 
 <script setup>
 import Breadcrumb from '@/components/Breadcrumb.vue';
+import WorkInProgress from '@/components/WorkInProgress.vue'
 </script>
