4fdee2fa6dc0823cbb833e2b49a28f44ea008cee
yeeway <yiwei.suuu@gmail.com>
2024-02-28 16:17:48 +0800

    Update forum page layout and text style

--- src/views/forum/LetterToHighSchoolStudents.vue
+++ src/views/forum/LetterToHighSchoolStudents.vue
@@ -10,13 +10,12 @@
     </div>
     <div v-else>
       <div
-        class="w-full py-12 sm:px-16 px-8 bg-white shadow-md mb-16 rounded bg-contain bg-repeat-y sm:text-lg text-sm"
+        class="w-full py-7 sm:px-16 px-8 bg-white shadow-md mb-16 rounded bg-repeat-y text-md"
         style="background-image: url('/BikeFestival17th-Frontend/forum_page/letter-note-texture.png');"
       >
         <!-- <div class="font-bold text-3xl text-content mb-8">{{ data.title }}</div> -->
         <!-- <img class="w-[500px] mx-auto my-8" src="/forum_page/LetterToHighSchoolStudents.webp"> -->
-        <p>To 各位高中生們,</p><br>
-        <div v-html="data.content"></div><br>
+        <div v-html="data.content" class="leading-[36px]"></div><br>
         <p class="text-right">From {{ data.author }}</p>
       </div>
     </div>
